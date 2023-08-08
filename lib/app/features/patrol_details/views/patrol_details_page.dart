c07b99aca7fcaf8e6d3b394275a641903ac25ccb
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2023-08-08 20:53:57 -0300

    chore: add patrol details page

---
+++ lib/app/features/patrol_details/views/patrol_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PatrolDetailsPage extends StatefulWidget {
+  const PatrolDetailsPage({super.key});
+
+  @override
+  State<PatrolDetailsPage> createState() => _PatrolDetailsPageState();
+}
+
+class _PatrolDetailsPageState extends State<PatrolDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
