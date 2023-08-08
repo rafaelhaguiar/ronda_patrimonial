07c3771e3a5836dc32bd4accd62059915ee033f0
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2023-08-08 20:57:04 -0300

    chore: add theme file

--- lib/app/config/main_widget.dart
+++ lib/app/config/main_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ronda_patrimonial/app/config/routes.dart';
+import 'package:ronda_patrimonial/app/config/theme.dart';
 
 class MainWidget extends StatelessWidget {
   const MainWidget({super.key});
@@ -7,6 +8,7 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: theme,
       initialRoute: Routes.splash,
       routes: Routes.allRoutes,
     );
