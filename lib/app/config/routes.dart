685696574b6106af914a0b21682577dfe259e589
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2023-08-08 20:55:06 -0300

    chore: add patrol details page in named routes

--- lib/app/config/routes.dart
+++ lib/app/config/routes.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:ronda_patrimonial/app/features/home/views/home_page.dart';
+import 'package:ronda_patrimonial/app/features/patrol_details/views/patrol_details_page.dart';
 import 'package:ronda_patrimonial/app/features/splash/views/splash_page.dart';
 
 final class Routes {
   static const String splash = '/';
   static const String home = 'home';
+  static const String patrolDetails = 'patrolDetails';
 
   static final allRoutes = <String, Widget Function(BuildContext)>{
     splash: (BuildContext context) => const SplashPage(),
     home: (BuildContext context) => const HomePage(),
+    patrolDetails: (BuildContext context) => const PatrolDetailsPage(),
   };
 }
